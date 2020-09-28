197960d012360f826173928f3e0f1b35cf413363
Johnny <name.johnny@mail.ru>
2020-09-28 11:47:46 +0300

    TODO: debug

--- codec.cpp
+++ codec.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <boost/dynamic_bitset.hpp>
+#include <cmath>
 
 #define ASCII_OFFSET 47
 #define ERROR 1
@@ -12,6 +13,7 @@ class Codec {
 
     private:
         float accuracy;
+        boost::dynamic_bitset<> dec_remainder;
         boost::dynamic_bitset<> polynomial;
         boost::dynamic_bitset<> message;
         boost::dynamic_bitset<> vector_a;
@@ -19,10 +21,12 @@ class Codec {
         boost::dynamic_bitset<> vector_b;
 
         uint get_index_of_high_1(boost::dynamic_bitset<>); // TODO
-        void run_sim(); // TODO
+        void run_sim();
         int init_values(std::string*, uint);
         void encode();
-        void decode(); // TODO
+        void decode();
+        void send_through_the_channel();
+        bool is_error(); // TODO
 };
 
 Codec::Codec() {};
@@ -37,7 +41,7 @@ int Codec::init_values(std::string *g_x, uint l) {
     bool start_null_flag = true;
     for (auto it = g_x->begin(); it != g_x->end(); it++) {
         if (*it != '1' && *it != '0') {
-            std::cout << "Wrong sequence. Please, enter the binary number.\n";
+            std::cout << "Wrong sequence. Please, enter the binary number.\n\n";
             return ERROR;
         }
         else {
@@ -64,6 +68,36 @@ void Codec::encode() {
     vector_a.operator|=(remainder);
 };
 
+void Codec::decode() {
+    dec_remainder = vector_b.operator<<(3);
+    while (dec_remainder.to_ulong() > polynomial.to_ulong()) {
+        int mul_digree = get_index_of_high_1(dec_remainder) - get_index_of_high_1(polynomial);
+        boost::dynamic_bitset<> tmp = dec_remainder.operator<<(mul_digree);
+        dec_remainder.operator&=(tmp);
+    }
+};
+
+void Codec::send_through_the_channel() {
+    vector_b.operator=(vector_a);
+    vector_b.operator^=(vector_e);
+};
+
+void Codec::run_sim() {
+    uint N = 9 / (4 * pow(accuracy, 2));
+    uint N_err = 0;
+    for (uint i = 0; i < N; i++) {
+        int rand_num = rand() % (vector_a.size() - polynomial.size());
+        message = boost::dynamic_bitset<> (message.size(), rand_num);
+        encode();
+        send_through_the_channel();
+        decode();
+        if (is_error())
+            N_err++;
+    }
+    double P_err = N_err / N;
+    std::cout << "Probability of decoding error when transmitting data over a binary-symmetric channel: " << P_err << "\n";
+};
+
 void Codec::start() {
     greeting();
 
@@ -79,7 +113,7 @@ void Codec::start() {
         if (init_values(&g_x, s_length) == ERROR)
             continue;
 
-        run_sim(); // TODO
+        run_sim();
 
         std::cout << "Do you want to exit? [y/n]: ";
         std::cin >> g_x;
