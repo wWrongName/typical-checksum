b5b223b98523f4d4dcb1b25969199c47b901250c
wWrongName <name.johnny@mail.ru>
2020-11-16 19:52:44 +0300

    clear code

--- codec.cpp
+++ codec.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include "C:\\Program Files\\mingw-w64\\x86_64-8.1.0-win32-seh-rt_v6-rev0\\mingw64\\lib\\gcc\\x86_64-w64-mingw32\\8.1.0\\include\\c++\\dynamic_bitset.hpp"
 #include <cmath>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
 #include <map>
+
+using namespace std;
 
 #define uint unsigned int
 #define ASCII_OFFSET 48
 #define ERROR 1
+
+int factorial(int number) {
+    return (number < 0) ? 0 : ((number < 2) ? 1 : number * factorial(number - 1));
+};
+
+double count_combination(int bottom, int top) {  // n, i
+    return (double)factorial(bottom) / (double)(factorial(bottom - top) * factorial(top));
+};
 
 class Codec {
     public:
@@ -14,29 +28,29 @@ class Codec {
         void greeting();
 
     private:
-        float accuracy;
         uint r;
         uint d;
         float p;
+        ofstream fout;
         boost::dynamic_bitset<> dec_remainder;
         boost::dynamic_bitset<> polynomial;
         boost::dynamic_bitset<> message;
         boost::dynamic_bitset<> vector_a;
         boost::dynamic_bitset<> vector_e;
         boost::dynamic_bitset<> vector_b;
-        std::map<int, int> A_weights; // A <- {key = weight, value = volume}
+        vector<string> data;
+        map<int, int> A_weights; // A <- {key = weight, value = volume}
 
-        uint get_index_of_high_1(boost::dynamic_bitset<>);
-        int init_values(std::string*, uint);
-        void count_weigth_of_A_words();
-        bool is_error();
-
-        void run_sim();
         boost::dynamic_bitset<> encode(boost::dynamic_bitset<>);
-        void decode();
+        uint get_index_of_high_1(boost::dynamic_bitset<>);
+        int init_values(string*, uint);
+        void count_weigth_of_A_words();
         void send_through_the_channel();
-        void up_prob_of_dec_err();
         void acc_prob_of_dec_err();
+        void up_prob_of_dec_err();
+        void run_sim();
+        void decode();
+        bool is_error();
 };
 
 Codec::Codec() {};
@@ -44,16 +58,15 @@ Codec::Codec() {};
 void Codec::count_weigth_of_A_words() {
     boost::dynamic_bitset<> A_word(vector_a.size() - r);
     for (int i = 0; i < pow(2, vector_a.size() - r); i++) {
-        // std::cout << A_word << "\n";
         auto tmp = A_word;
         for (uint i = 0; i < r; i++)
             tmp.push_back(0);
         int weight = encode(tmp).count();
-        // std::cout << A_weights[weight] << "\n";
+
         if (A_weights.find(weight) != A_weights.end())
             A_weights.at(weight)++;
         else
-            A_weights.insert(std::pair<int, int>(weight, 1));
+            A_weights.insert(pair<int, int>(weight, 1));
 
         for (int i = 0; i < A_word.size(); i++) {
             A_word.flip(i);
@@ -61,9 +74,6 @@ void Codec::count_weigth_of_A_words() {
                 break;
         }
     }
-    // for (auto it = A_weights.begin(); it != A_weights.end(); ++it) {
-    //     std::cout << (*it).first << " : " << (*it).second << "\n";
-    // }
 }
 
 uint Codec::get_index_of_high_1(boost::dynamic_bitset<> b_s) {
@@ -77,19 +87,11 @@ bool Codec::is_error() {
     return (vector_e.any() && dec_remainder.none()) ? true : false;
 };
 
-int Codec::init_values(std::string *g_x, uint l) {
-    if (accuracy <= 0 || accuracy > 1) {
-        std::cout << "Wrong accuracy. Please, enter some value in the range 0 < accuracy <= 1.\n\n";
-        return ERROR;
-    }
-    if (p <= 0 || p > 1) {
-        std::cout << "Wrong accuracy. Please, enter some value in the range 0 < accuracy <= 1.\n\n";
-        return ERROR;
-    }
+int Codec::init_values(string *g_x, uint l) {
     bool start_null_flag = true;
     for (auto it = g_x->end() - 1; it != g_x->begin(); it--) {
         if (*it != '1' && *it != '0') {
-            std::cout << "Wrong sequence. Please, enter the binary number.\n\n";
+            cout << "Wrong sequence. Please, enter the binary number.\n\n";
             return ERROR;
         }
         else {
@@ -141,21 +143,14 @@ void Codec::send_through_the_channel() {
     vector_b.operator^=(vector_e);
 };
 
-int factorial(int number) {
-    return (number < 0) ? 0 : ((number < 2) ? 1 : number * factorial(number - 1));
-};
-
-double count_combination(int bottom, int top) {  // n, i
-    return (double)factorial(bottom) / (double)(factorial(bottom - top) * factorial(top));
-};
-
 void Codec::acc_prob_of_dec_err() {
     count_weigth_of_A_words();
     double P_err = 0.0;
     int n = vector_a.size();
     for (int i = d; i <= n; i++)
         P_err += A_weights[i] * pow(p, i) * pow(1 - p, n - i);
-    std::cout << "Accurate probability of decoding error: " << P_err << "\n";
+    data.push_back(to_string(p));
+    data.at(data.size() - 1) += (" " + to_string(P_err));
 };
 
 void Codec::up_prob_of_dec_err() {
@@ -164,10 +159,67 @@ void Codec::up_prob_of_dec_err() {
     for (uint i = 0; i < d - 1; i++) {
         P_err += count_combination(n, i) * pow(p, i) * pow(1 - p, n - i);
     }
-    std::cout << "Upper probability of decoding error: " << 1 - P_err << "\n";
+    data.at(data.size() - 1) += (" " + to_string(1 - P_err));
 };
 
 void Codec::run_sim() {
+    for (p = 0; p <= 1; p += 0.01) {
+        acc_prob_of_dec_err();
+        up_prob_of_dec_err();
+        A_weights.clear();
+    }
+    for (auto it = data.begin(); it != data.end(); it++) {
+        fout << *it << "\n";
+    }
+};
+
+void Codec::greeting() {
+    cout << "Program for calculating the probability of decoding error\n";
+    cout << "------------------------------------------------------------------------------------------------------\n\n";
+};
+
+void Codec::start() {
+    greeting();
+    fout.open("data.txt", ios_base::out);
+    if (!fout.is_open()) {
+        return;
+    }
+    string g_x;
+    uint s_length;
+    while (true) {
+        cout << "Please, write the generating polynomial [g(x)]: ";
+        cin >> g_x;
+        cout << "Please, write the length of the sequence [l]: ";
+        cin >> s_length;
+        cout << "Please, write the distance of the code [d]: ";
+        cin >> d;
+        if (init_values(&g_x, s_length) != ERROR)
+            run_sim();
+    question:
+        cout << "Do you want to exit? [y/n]: ";
+        cin >> g_x;
+        if (g_x == "y" || g_x == "Y") {
+            cout << "Bye!\n\n";
+            break;
+        }
+        else if (g_x == "n" || g_x == "N") {
+        }
+        else {
+            goto question;
+        }
+        cout << "\n";   
+    }
+    fout.close();
+};
+
+int main(int argc, char **argv) {
+    Codec codec;
+    codec.start();
+    return 0;
+};
+
+// run_sim
+/* 
     uint N = ((double)9 / (double)(4 * pow(accuracy, 2))) + 0.5;
     uint N_err = 0;
     for (uint i = 0; i < N; i++) {
@@ -183,54 +235,5 @@ void Codec::run_sim() {
         }
     }
     double P_err = (double)N_err / (double)N;
-    std::cout << "Probability of decoding error when transmitting data over a binary-symmetric channel: " << P_err << "\n";
-};
-
-void Codec::greeting() {
-    std::cout << "Program for calculating the probability of decoding error\n";
-    std::cout << "------------------------------------------------------------------------------------------------------\n\n";
-};
-
-void Codec::start() {
-    greeting();
-
-    std::string g_x;
-    uint s_length;
-    while (true) {
-        std::cout << "Please, write the generating polynomial [g(x)]: ";
-        std::cin >> g_x;
-        std::cout << "Please, write the length of the sequence [l]: ";
-        std::cin >> s_length;
-        std::cout << "Please, write the accuracy [epsilon]: ";
-        std::cin >> accuracy;
-        std::cout << "Please, write the pobabity of the error [p]: ";
-        std::cin >> p;
-        std::cout << "Please, write the distance of the code [d]: ";
-        std::cin >> d;
-        if (init_values(&g_x, s_length) != ERROR)
-            run_sim();
-        up_prob_of_dec_err();
-        acc_prob_of_dec_err();
-question:
-        std::cout << "Do you want to exit? [y/n]: ";
-        std::cin >> g_x;
-        if (g_x == "y" || g_x == "Y") {
-            std::cout << "Bye!\n\n";
-            break;
-        }
-        else if (g_x == "n" || g_x == "N") {
-        }
-        else {
-            goto question;
-        }
-        std::cout << "\n";
-    }
-};
-
-int main(int argc, char **argv) {
-
-    Codec codec;
-    codec.start();
-
-    return 0;
-};
+    cout << "Probability of decoding error when transmitting data over a binary-symmetric channel: " << P_err << "\n"; 
+*/
